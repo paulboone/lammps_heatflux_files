0737ada75c227b4b12ce5258805cee43f6429a79
Paul Boone <narcissus.pong@gmail.com>
2018-01-18 11:16:57 -0500

    last periodic crystal commits

--- periodic_crystal/mofs.py
+++ periodic_crystal/mofs.py
@@ -29,13 +29,20 @@ def cubic_45_degrees_def(extend_xyz, linker_length=10):
 
 def triangular_def(extend_xyz, linker_length=10.0):
     bl = linker_length / 3
-    sq3 = sqrt(3.0)
+    sq3 = np.sqrt(3.0)
     atoms = np.array([(0, 0, 0, 0), (1, bl, 0, 0), (2, -bl, 0, 0),
              (3, bl/2, bl*sq3/2, 0), (4, bl, bl*sq3, 0),
              (5, -bl/2, bl*sq3/2, 0), (6, -bl, bl*sq3, 0),
-             (7, 0, 0, bl), (7, 0, 0, 2*bl)]) + (0, 5, 0, 0)
-    int_bonds = np.array([(0,1), (0,2), (0,3), (3,4), (0,5), (5,6), (0,7), (7,8)])
-    ext_bonds = np.array([[(1,2), (4, 0)]])
+             (7, -bl/2, -bl*sq3/2, 0), (8, -bl, -bl*sq3, 0),
+             (9, bl/2, -bl*sq3/2, 0), (10, bl, -bl*sq3, 0),
+             (11, 3*bl/2, -3*bl*sq3/2, 0), (12, bl/2, -3*bl*sq3/2, 0), (13, -bl/2, -3*bl*sq3/2, 0),
+             (14, 0, 0, bl), (15, 0, 0, 2*bl),
+             (16,  3*bl/2, -3*bl*sq3/2, bl), (17,  3*bl/2, -3*bl*sq3/2, 2*bl)]) \
+              + (0, 5, 0, 0)
+    int_bonds = np.array([(0,1), (0,2), (0,3), (3,4), (0,5), (5,6), (0,7), (7,8), (0,9), (9,10),
+                         (10,11), (11,12), (12,13), (0,14), (14,15), (0,14), (14,15), (11,16), (16,17)])
+    ext_bonds = np.array([[(1,2),(1,0,0)], [(4, 11), (0,1,0)], [(6,11),(-1,1,0)], [(11,8),(1,0,0)],
+                         [(11,13),(1,0,0)], [(15,0),(0,0,1)], [(17,11), (0,0,1)]])
     box_bounds = np.array([linker_length, linker_length, linker_length])
 
     return atoms, int_bonds, ext_bonds, box_bounds, extend_xyz
--- periodic_crystal/tests/test_extend.py
+++ periodic_crystal/tests/test_extend.py
@@ -28,13 +28,18 @@ def test_extend_cubic_45_degrees_1x1x2():
     assert(set(map(tuple,all_bonds)) == set([(0, 1), (1, 2), (0, 3), (3, 4), (0, 5), (5, 6), (2, 0), (4, 0),
         (6, 7), (7, 8), (8, 9), (7, 10), (10, 11), (7, 12), (12, 13), (9, 7), (11, 7), (13, 0)]))
 
-@pytest.mark.skip
+
 def test_extend_triangular_1x1x2():
-    atoms, int_bonds, ext_bonds, box_bounds, extend_xyz = pc.mofs.triangular_def((1,1,2))
+    atoms, int_bonds, ext_bonds, box_bounds, extend_xyz = pc.mofs.triangular_def((1,2,1))
     all_atoms, all_bonds = pc.extend(atoms, int_bonds, ext_bonds, box_bounds, extend_xyz)
-    pass
-    assert(set(map(tuple,all_bonds)) == set([(0, 1), (1, 2), (0, 3), (3, 4), (0, 5), (5, 6), (2, 0), (4, 0),
-        (6, 7), (7, 8), (8, 9), (7, 10), (10, 11), (7, 12), (12, 13), (9, 7), (11, 7), (13, 0)]))
+    print(map(tuple, all_bonds))
+    assert(set(map(set, all_bonds)) == map(set,set([
+    (0, 1), (0, 2), (0, 3), (3, 4), (0, 5), (5, 6), (0, 7), (7, 8), (0, 9), (9, 10), (10, 11), \
+     (11, 12), (12, 13), (0, 14), (14, 15), (11, 16), (16, 17), \
+    (18, 19), (18, 20), (18, 21), (21, 22), (18, 23), (23, 24), (18, 25), (25, 26), (18, 27), \
+     (27, 28), (28, 29), (29, 30), (30, 31), (18, 32), (32, 33), (29, 34), (34, 35), \
+    (11,22), (11,8), (11,13), (11,24), (11,17), (29,4), (29,26), (29,31), (29,6), (29,35), \
+    (1,2), (19,20), (0,14), (18,32)])))
 
 @pytest.mark.skip
 def test_extend_hexagonal_1x1x2():
