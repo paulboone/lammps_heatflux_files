44adb4b4de6d3a2f94c731086ee4df1a5203fae0
Paul Boone <narcissus.pong@gmail.com>
2018-01-19 15:20:26 -0500

    octane should be mostly working now–ready for first test

--- hydrocarbons/octane VMD tests/vmd-octane.tcl
+++ hydrocarbons/octane VMD tests/vmd-octane.tcl
@@ -1,7 +1,7 @@
 package require topotools
 package require pbctools
 
-mol new octane.xyz autobonds no waitfor all
+mol new octane_packed.xyz autobonds no waitfor all
 
 set sel_ch3 [atomselect top "type H"]
 set sel_ch2 [atomselect top "type He"]
@@ -13,7 +13,7 @@ $sel_ch3 set radius 1.3
 
 $sel_ch2 set name "CH2"
 $sel_ch2 set type "CH2"
-$sel_ch3 set mass 14.027
+$sel_ch2 set mass 14.027
 $sel_ch2 set radius 1.3
 
 mol bondsrecalc top
@@ -21,5 +21,5 @@ topo guessangles
 topo guessdihedrals
 mol reanalyze top
 
-pbc set {0.0 0.0 0.0 20.0 20.0 20.0}
+pbc set {25.84 25.84 232.57 90.0 90.0 90.0}
 topo writelammpsdata octane.data full
