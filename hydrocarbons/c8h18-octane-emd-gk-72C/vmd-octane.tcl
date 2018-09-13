7348b6d233357258eeeec2d50bb08381ff5de03e
Paul Boone <narcissus.pong@gmail.com>
2018-09-13 10:20:57 -0400

    updated octane gk with 25.84 cubic simulation box

---
+++ hydrocarbons/c8h18-octane-emd-gk-72C/vmd-octane.tcl
@@ -0,0 +1,27 @@
+package require topotools
+package require pbctools
+
+mol new octane_packed.xyz autobonds no waitfor all
+
+set sel_ch3 [atomselect top "type H"]
+set sel_ch2 [atomselect top "type He"]
+
+$sel_ch3 set name "CH3"
+$sel_ch3 set type "CH3"
+$sel_ch3 set mass 15.035
+$sel_ch3 set radius 1.3
+
+$sel_ch2 set name "CH2"
+$sel_ch2 set type "CH2"
+$sel_ch2 set mass 14.027
+$sel_ch2 set radius 1.3
+
+mol bondsrecalc top
+topo guessangles
+topo guessdihedrals
+mol reanalyze top
+
+pbc set {25.84 25.84 25.84 90.0 90.0 90.0}
+topo writelammpsdata octane.data full
+
+exit 0
